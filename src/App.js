d9834c681939e2c2872c399249c7c9013b7d6bef
zoe kelman <zoey247@aol.com>
2019-04-18 12:50:14 +0100

    Work done in lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 class App extends Component {
+
+
   render() {
+
+    const headers = ["Todoapp", "One", "Two", "Three", "Four"];
+
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        {
+          headers.map(function (header, index) {
+            return <Header headerText={header} Key={index} />;
+          })
+        }
+
       </div>
     );
   }
