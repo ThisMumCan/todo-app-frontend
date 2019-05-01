33f99f7ca190a837c1f58a40da076278ce5c0257
zoe kelman <zoey247@aol.com>
2019-05-01 11:48:46 +0100

    Corrected form class

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -21,11 +21,11 @@ class AddItem extends Component {
     render() {
         return (
             <div className="Row AddItem">
-                <div class="row align-items-center">
+                <div className="row align-items-center">
                     <div className="col-5 inputbox">
                         <form>
-                            <input type="text" class="form-control" placeholder="Add item to the list" onChange={this.inputBoxChanged}></input>
-                            <div class="input-group-btn">
+                            <input type="text" classname="form-control" placeholder="Add item to the list" onChange={this.inputBoxChanged}></input>
+                            <div classname="input-group-btn">
                                 <button className="btn btn-default" type="submit">
                                     <i className="glyphicon glyphicon-plus" onSubmit={this.state.addItemClicked}></i>
                                 </button>
